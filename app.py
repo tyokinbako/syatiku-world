a30bb1375e90fd736cce3ff4a3dd818b4de483d9
tyokinbako <haruharu0523.rururu@gmail.com>
2025-08-03 18:56:00 +0900

    new

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def santa():
 def hello():
     return render_template("index.html")
 
+@app.route("/riyoukiyaku")
+def unti():
+    return render_template("riyoukiyaku.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
