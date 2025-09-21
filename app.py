974c3392e4cf0c2f4497e121e97d4e4d1701e3b8
tyokinbako <haruharu0523.rururu@gmail.com>
2025-09-21 17:42:15 +0900

    aa

--- app.py
+++ app.py
@@ -24,5 +24,58 @@ def aaaa():
 def aaaaaaaaaw():
     return render_template("sutezerihu.html")
 
+@app.route("/pdf")
+def pdf():
+    from reportlab.pdfbase import pdfmetrics
+    from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+    # 日本語フォントを登録（ここでは明朝体 HeiseiMin-W3）
+    pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3'))
+
+    from reportlab.pdfgen import canvas
+
+    from io import BytesIO
+
+    from flask import send_file
+
+    # PDFファイルの保存先
+    file_name = "aiueonzzzz.pdf"
+
+    # キャンバスを作成
+    buf = BytesIO()
+
+    c = canvas.Canvas(buf)
+
+    c.setFont("HeiseiMin-W3", 20)
+
+    # テキストを描画
+    c.drawString(30, 700,"　　　御中")
+    c.setFont("HeiseiMin-W3", 50)
+    c.drawString(230, 780, "辞任届")
+    c.setFont("HeiseiMin-W3", 15)
+    c.drawString(30, 670, "氏名 ")
+    c.drawString(400, 670, "辞任理由")
+    c.drawString(460, 670, "　　　　")
+    c.drawString(405, 650, "今までの感謝")
+    c.drawString(500, 650, "　　")
+    c.setFont("HeiseiMin-W3", 10)
+    c.drawString(30, 550, "宛名       　様、ありがとうございました")
+    x, y = 30, 430  # 左下の座標
+    width, height = 200, 100  # 幅と高さ
+    c.rect(x, y, width, height, stroke=1, fill=0)  # stroke=枠線, fill=塗りつぶし
+    # PDFを保存
+    c.save()
+
+    buf.seek(0)
+    response = send_file(
+        buf,
+        mimetype="application/pdf",
+        as_attachment=True,
+        download_name=file_name
+    )
+    response.headers["Cache-Control"] = "no-store"
+    return response
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
